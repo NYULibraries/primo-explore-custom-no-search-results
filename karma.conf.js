8b0a28cba3026bd336286a266e2168a4a8048ad9
Elliott Grieco <elliott.grieco@gmail.com>
2018-06-19 17:47:43 -0400

    Configures for transpilation

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,33 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage', 'coveralls'],
     browsers: ['PhantomJS'],
+    basePath: 'src/',
     files: [
-      //libraries
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      //module
+      '../node_modules/angular/angular.js',
+      '../node_modules/angular-mocks/angular-mocks.js',
       'js/**/*.js',
-      //tests
-      'spec/**/*.js',
-      //external templates
-      'spec/**/*.html',
+      'spec/**/*.spec.js',
     ],
     preprocessors: {
-      'js/**/*.js': ['babel'],
-      'spec/**/*.spec.js': ['babel'],
-      'spec/**/*.html': ['ng-html2js'],
+      'js/**/*.js': ['webpack', 'sourcemap'],
+      'spec/**/*.spec.js': ['babel', 'sourcemap'],
     },
-    ngHtml2JsPreprocessor: {
-      stripPrefix: "spec/",
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      devtool: 'inline-source-map'
     }
   });
 };
